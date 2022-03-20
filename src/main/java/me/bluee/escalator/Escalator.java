5af35da3e329c32aa23e176be50ac7ea6f3946da
HisokaBluee <mattias.gervilliers@etu.univ-amu.fr>
2022-03-20 12:27:51 +0100

    Project creation

---
+++ src/main/java/me/bluee/escalator/Escalator.java
@@ -0,0 +1,17 @@
+package me.bluee.escalator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Escalator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
